e45eef94ba6c409adf0a0fdbe676dca5e80c4552
KONRURI <505076933@qq.com>
2016-11-03 14:40:19 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -14,20 +14,20 @@ import java.util.Scanner;
  * 3. 创建pull request，与主仓库的master分支对比
  */
 public class TimestampTransfer {
-	@SuppressWarnings("resource")
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			Date lineDate = null;
-			long lineTimestamp;
+	@SuppressWarnings("resource")//资源注入
+	public static void main(String[] args){ //main方法
+		Scanner scanner = new Scanner(System.in);//设置键盘输入 实例化调用方法
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//初始化并表明输入格式
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");//初始化并标明输出格式
+		while (scanner.hasNext()){//循环判断键盘输入
+			String line = scanner.nextLine();//获取输入 赋值给line
+			Date lineDate = null;//将lineDate赋值为null
+			long lineTimestamp;//将时间戳类型转换为line
 			try {
-				lineDate = inputFormat.parse(line);
-				lineTimestamp = lineDate.getTime();
-				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
-			} catch (ParseException e) {
+				lineDate = inputFormat.parse(line);//解析输入日期
+				lineTimestamp = lineDate.getTime();//得到时间戳时间 单位为毫秒
+				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);//输出时间格式为时间to时间戳
+			} catch (ParseException e) {//捕获异常
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
