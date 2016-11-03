aa147538ab2532896f060dceddbc664ca4aeb8ae
KONRURI <505076933@qq.com>
2016-11-03 16:51:46 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,17 @@
 package No4;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		HashSet<String> setVisitPaths = new HashSet<String>();
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
+			String[] tokens = line.split("\t");
+			setVisitPaths.add(tokens[0]);
+			System.out.println(setVisitPaths.size());
 		}
 	}
 }
