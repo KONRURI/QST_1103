6b845f76136e84b18ccd6a5a4d23a3faef8c626d
KONRURI <505076933@qq.com>
2016-11-03 15:39:19 +0800

    Update FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -5,7 +5,10 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /*
  * 题目要求：
@@ -21,18 +24,33 @@ public class FilterByTime {
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
+	
+		String filePath = "C:/Users/50507/Desktop./access.log";
 		FileInputStream inputStream = new FileInputStream(filePath);
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
+		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[([^ ]*) [^ ]*\\] \"([^ ]+) ([^ ]+) ([^ ]*)\" (\\d+) (\\d+) \"([^ ]+)\" \"(.*)\"";
+		//给定的正则表达式编译并赋予给Pattern类
+		Pattern r = Pattern.compile(pattern);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy:ss:mm:HH", Locale.US);
+
 		while (scanner.hasNext()){
 			// 对每行进行处理
 			String line = scanner.nextLine();
 			// 切分获取IP，Time
 			String strIp = null;
 			String strTime = null;
+			Matcher m = r.matcher(line);
+			if (m.find()) {
+				Date date = dateFormat.parse(m.group(2));
+				if (date.before(endDate) && date.after(beginDate)) {
+					strIp = m.group(1);
+					strTime = date.toString();
+
 			// 对在时间区间内的数据进行输出
 			System.out.println(strIp + "\t" + strTime);
+				}
+			}
+	
 		}
 	}
-	
 }
